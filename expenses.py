2340d17eb84d761ed7723a913a7d327d40346706
SilviaCabello <silviacabellom@gmail.com>
2021-05-21 16:03:08 +0200

    add a shorter way to sum the expenses

--- expenses.py
+++ expenses.py
@@ -4,4 +4,16 @@ sum = 0
 for x in expenses:
     sum = sum + x
 
-print(sum)    
+print("You spent $", sum, sep="")    
+
+
+ 
+ 
+# sum
+# expenses = [10.5, 8, 5, 15, 20, 5, 3]
+# total = sum(expenses)
+
+# print("You spent $", total, sep="")
+
+ 
+ 
