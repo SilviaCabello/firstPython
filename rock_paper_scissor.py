1c0be4ce5ec4ee83394ec5c57ef009af27e4665c
SilviaCabello <silviacabellom@gmail.com>
2021-05-21 15:28:00 +0200

    add the random module

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,14 +1,16 @@
-computer_choice = "scissors"
+import random 
+
+computer_choice = random.choice(["scissors", "rock", "paper"])
 
 user_choice = input("Do you want - rock, paper or scissors?\n")
 
 if computer_choice == user_choice:
-    print("Tie!")
+    print("Tie! The computer choose " + str(computer_choice))
 elif user_choice == "rock" and computer_choice == "scissors":
-    print("You win!")
+    print("You win! The computer choose " + str(computer_choice))
 elif user_choice == "paper" and computer_choice == "rock":
-    print("You win!")
+    print("You win! The computer choose " + str(computer_choice))
 elif user_choice == "scissors" and computer_choice == "paper":
-    print("You win!")
+    print("You win! The computer choose " + str(computer_choice))
 else:
-    print("You lose :(")
+    print("You lose :(. The computer choose " + str(computer_choice))
