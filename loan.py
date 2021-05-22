c52ef601885a4fd07c66e9e65b0a3eacc6bd8725
SilviaCabello <silviacabellom@gmail.com>
2021-05-22 11:59:03 +0200

    create a movie schedule with a dictionary

---
+++ loan.py
@@ -0,0 +1,4 @@
+money_owed = float(input("How much money do you owe, in dollars?\n"))
+apr = float(input("What is the annual percentage rate?\n"))
+payment = float (input("What will your monthly payment be, in dollars?\n"))
+months = int(input("How many months do you want to see resulst for?\n"))
