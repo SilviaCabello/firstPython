b608c328b043bb266d6383de78724694fe73d3cc
SilviaCabello <silviacabellom@gmail.com>
2021-05-24 11:05:27 +0200

    change units to metric

--- weather.py
+++ weather.py
@@ -2,7 +2,7 @@ import requests
 
 api_key = "08bc139c456596450dfe0769403f6c5e"
 city = "Valencia"
-url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key+""
+url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key+"&units=metric"
 
 request = requests.get(url)
 json = request.json()
